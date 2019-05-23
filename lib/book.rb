7972ae616dfb269194b30248faea353c6e358516
Margot Konig <margot.konig@blackrock.com>
2019-05-23 02:52:40 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,12 @@
-
-
+class Book
+  attr_accessor :title, :author, :page_count, :genre
+  def initialize(title)
+    @book_title = title
+  end
+  def title
+    @book_title
+  end
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
