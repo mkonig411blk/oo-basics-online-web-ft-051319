8118ec955a421bf3f299d54126b60170adccc24e
Margot Konig <margot.konig@blackrock.com>
2019-05-23 02:40:09 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,5 +1,5 @@
 class Book
-  attr_accessor :title :author_name :page_count :genre
+  attr_accessor :title, :author_name, :page_count, :genre
   def initializer(title)
     @book_title = title
   end
